cebe19929930e02377ea12b19370da72b3497093
Burak Kuru <burak.kuru@abkholding.com>
2022-10-12 18:24:15 +0300

    fixes for main

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -6,17 +6,16 @@ import (
 )
 
 func main() {
-	println("Hello, world!")
 
-	//sorts a bunch of words by the number of character “a”s within the
-
+	//case 1: sorts a bunch of words by the number of character “a”s within the
 	sorted := funcs.SortBunchOfWordsByTheNumberOfCharacterA([]string{"aaaasd", "a", "aab", "aaabcd", "ef", "cssssssd", "fdz", "kf", "zc", "lklklklklklklklkl", "l"})
-
 	fmt.Printf("%v\n", sorted)
 
-	mostRepeated := funcs.FindMostRepeatedDataInArray([]string{"apple", "pie", "apple", "red", "red", "red"})
+	//case2: the algorithm is x!+x
+	funcs.MyBrilliantFunction(9)
 
+	//case3: find the most repeated word in a bunch of words
+	mostRepeated := funcs.FindMostRepeatedDataInArray([]string{"apple", "pie", "apple", "red", "red", "red"})
 	fmt.Printf("%v\n", mostRepeated)
 
-	funcs.MyBrilliantFunction(9)
 }
